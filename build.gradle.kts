a2ff580827daff0c491c0bb926e3bbc0e169c77b
Tofik <slodkitofik@gmail.com>
2021-03-03 21:28:42 +0100

    Make this a working java program

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-group = "me.minidigger"
-version = "1.0-SNAPSHOT"
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     java
     application
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 java {
@@ -18,5 +18,26 @@ repositories {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.guava:guava:30.1-jre")
     testImplementation("junit:junit:5.7.1")
 }
+
+project.setProperty("mainClassName", "me.minidigger.spigotresolver.SpigotResolver")
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("shadow")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "me.minidigger.spigotresolver.SpigotResolver"))
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+
